33855df0375bdb44dbbc8074dd864232d35a8a3b
SharpIce <VupRbl@163.com>
2024-10-10 19:17:40 +0800

    字体优化

--- src/vue/home.vue
+++ src/vue/home.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="opacity-animation font-serif flex flex-col justify-center space-y-4 font-bold">
+	<div class="opacity-animation flex flex-col justify-center space-y-4">
 		<div class="space-y-4 rounded-lg border border-nord1 p-4 max-md:border-none">
 			<!-- 个人信息 -->
 			<div class="paragraph">
