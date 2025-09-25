783eb42bf829456e67932f7210475ac5c19f81ad
SharpIce <VupRbl@163.com>
2025-09-25 08:34:37 +0800

    feat: 更新依赖
    Signed-off-by: SharpIce <VupRbl@163.com>

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,13 +1,15 @@
 import globals from 'globals';
 import eslint from '@eslint/js';
 import jsdoc from 'eslint-plugin-jsdoc';
+import tseslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
 import vueParser from 'vue-eslint-parser';
+import { defineConfig } from 'eslint/config';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
-import tseslint, { type InfiniteDepthConfigWithExtends } from 'typescript-eslint';
+import type { ConfigWithExtends } from '@eslint/config-helpers';
 
-const TypeScriptConfig: InfiniteDepthConfigWithExtends = {
+const TypeScriptConfig: ConfigWithExtends = {
 	extends: [tseslint.configs.strict, tseslint.configs.stylistic, jsdoc.configs['flat/recommended-typescript']],
 	languageOptions: {
 		parser: tseslint.parser,
@@ -22,7 +24,7 @@ const TypeScriptConfig: InfiniteDepthConfigWithExtends = {
 	},
 };
 
-const config: InfiniteDepthConfigWithExtends = tseslint.config(
+const config = defineConfig(
 	eslint.configs.recommended,
 	eslintPluginUnicorn.configs.recommended,
 	tseslint.configs.strict,
